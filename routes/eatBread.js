7bf52125a189aeea733133a474e3087325bc7101
heshiyu2015 <7682468@163.com>
2014-11-24 12:25:12 +0800

    now I can get what I want

--- routes/eatBread.js
+++ routes/eatBread.js
@@ -1,13 +1,19 @@
 var express = require('express');
+var cheerio = require('cheerio');
 var router = express.Router();
-var url = 'http://breadtrip.com/trips/2387754590/';
 var doEatBread = require('../lib/doEatBread');
 
 router.get('/',function(req,res,next){
+  var id = req.param('id');
+  var url = 'http://breadtrip.com/trips/' + id + '/';
   doEatBread(url,function(err,data){
     if(err) return next(err);
-    console.log(data);
-  });
+    var $ = cheerio.load(data);
+    var title = $('div.trip-summary>h2')[0].attribs.title;
+    
+    res.writeHead(200,{'Content-Type':'text/plain;charset=utf-8'});
+    res.end(title);
+  }); 
 });
 
 module.exports = router;
