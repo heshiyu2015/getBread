d7466421d841e84e9f537789d0d7e799b0494b0e
heshiyu2015 <7682468@163.com>
2014-11-25 18:13:46 +0800

    almost that

--- routes/eatBread.js
+++ routes/eatBread.js
@@ -7,12 +7,18 @@ router.get('/',function(req,res,next){
   var id = req.param('id');
   var url = 'http://breadtrip.com/trips/' + id + '/';
   var theseDays = [];//装载游记的内容发送给模板
+  var comments = [];//装载游记中的收藏总数，评论总数，分享数
   doEatBread(url,function(err,data){//获得这个url对应的游记的HTML
     if(err) return next(err);
     var $ = cheerio.load(data);
     var title = $('div#trip-info>div.trip-summary>h2').attr('title');//标题
     var author_img = $('div#trip-info>a.trip-user>img').attr('src');//作者的头像链接
     var author_name = $('div#trip-info>a.trip-user>img').attr('alt');//作者名称
+    var total_num = $('div#trip-info>div.trip-tools>a');//找出收藏总数，评论总数，分享数的合集
+    
+    total_num.each(function(i,ccs){//装载收藏总数，评论总数，分享总数
+      comments.push($(ccs).find('b').text());
+    });
     
     var days = $('div.trip-wps').find('div.trip-days');//找出这篇游记中所有天的内容
 
@@ -29,14 +35,19 @@ router.get('/',function(req,res,next){
 
       var picAndCont = $(day).find('div.waypoint');//找出这一天的文字图片分组
       picAndCont.each(function(i,pc){
-        var pictureAndContent = [];//装载这一天的图片和文字
+        var pictureAndContent = [];//装载这一天的一组图片和文字等信息
         var dataForpc = $(pc).find('div.photo-ctn>a');//找出分组中的文字和图片
         var time = $(pc).find('div.time').text();//图片文字的具体时间
         var address = $(pc).find('a.wp-poi-name>span').text();//图片文字的具体地点
+        var lcs = $(pc).find('div.wp-btns>a');
         pictureAndContent.push(dataForpc.attr('data-caption'));//装入文字
         pictureAndContent.push(dataForpc.attr('href'));//装入图片
         pictureAndContent.push(time);//装入具体时间
         pictureAndContent.push(address);//装入具体地址
+        lcs.each(function(i,num){//装载每条评论的喜欢数目，评论数目，分享数目
+          console.log($(num));
+          pictureAndContent.push($(num).text());
+        });
         oneDay.push(pictureAndContent);//装入这一天数组中
       });
 
@@ -47,7 +58,8 @@ router.get('/',function(req,res,next){
       author_img:author_img,
       author_name:author_name,
       num_of_days:num_of_days,
-      theseDays:theseDays
+      theseDays:theseDays,
+      comments:comments
     });
   }); 
 });
